a1baa79ec5d07b3e845727e69ad99b3320b8ea59
FILES=0 <files.eq.zero@gmail.com>
2019-06-18 13:11:21 +0900

    basic functions

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,43 @@
-import * as Botkit from 'botkit';
+import dotenv from 'dotenv';
+import { Botkit } from 'botkit';
+// import { SlackAdapter } from 'botbuilder-adapter-slack'
+import { SlackAdapter, SlackMessageTypeMiddleware } from 'botbuilder-adapter-slack';
 
-if (!process.env.token) {
-    console.log('Error: Specify token in environment');
+// Botkit for Slack Class Reference
+// https://botkit.ai/docs/v4/reference/slack.html
+// BotKit 4.xでのライブラリ仕様変更について
+// https://qiita.com/0622okakyo/items/07d1961697a4c620380a
+
+dotenv.config();
+
+if (!process.env.SLACK_CLIENT_ID) {
+    console.log('Error: Specify SLACK_BOTTOKEN in environment');
     process.exit(1);
 }
-console.log(process.env.token);
+console.log(process.env.SLACK_CLIENT_ID);
 
-const controller = Botkit.slackbot({
-    debug: false
+const adapter = new SlackAdapter({
+    // clientId: process.env.SLACK_CLIENT_ID,
+    clientSigningSecret: process.env.SLACK_SIGNING_SECRET,
+    botToken: process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN,
+    redirectUri: process.env.SLACK_REDIRECTURI || "",
+}).use(new SlackMessageTypeMiddleware());
+
+const controller = new Botkit({
+    adapter: adapter
 });
 
-const slackConfig: Botkit.SlackSpawnConfiguration = {
-    token: process.env.token || ""
-};
-const bot = controller.spawn(slackConfig);
-function connect() {
-    bot.startRTM((err) => {
-        if (err) {
-            console.log("Error==>" + err)
-            throw new Error(err);
-        }
-    });
-}
-connect();
-controller.on('rtm_close', function (bot) {
-    console.log('rtm_close-->RTM connection is closed');
-    connect();
+controller.on('message', async (bot, message) => {
+    await bot.reply(message, 'I heard a message!');
 });
 
 // say hi
-controller.hears('hi', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-    bot.reply(message, 'hello');
+controller.hears('hi', ['direct_message', 'direct_mention', 'mention'], async (bot, message) => {
+    await bot.reply(message, 'hello');
 });
 
 // default
 // 最後に記述してください。
-controller.hears('(.*)', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-    bot.reply(message, 'なに??');
+controller.hears('(.*)', ['direct_message', 'direct_mention', 'mention'], async (bot, message) => {
+    await bot.reply(message, 'なに??');
 });
