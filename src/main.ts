4c2eddd48c1a9cc44d07e03a754a83dc1845e65d
FILES=0 <files.eq.zero@gmail.com>
2019-06-18 14:39:14 +0900

    add test method

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 import { Botkit } from 'botkit';
 // import { SlackAdapter } from 'botbuilder-adapter-slack'
-import { SlackAdapter, SlackMessageTypeMiddleware } from 'botbuilder-adapter-slack';
+import { SlackAdapter, SlackMessageTypeMiddleware, SlackBotWorker } from 'botbuilder-adapter-slack';
+import { TestAdapter } from 'botbuilder';
 
 // Botkit for Slack Class Reference
 // https://botkit.ai/docs/v4/reference/slack.html
@@ -41,3 +42,26 @@ controller.hears('hi', ['direct_message', 'direct_mention', 'mention'], async (b
 controller.hears('(.*)', ['direct_message', 'direct_mention', 'mention'], async (bot, message) => {
     await bot.reply(message, 'なに??');
 });
+
+async function test(): Promise<void> {
+    // spawn a bot
+    let bot: SlackBotWorker = await controller.spawn() as SlackBotWorker;
+
+    await bot.startConversationInChannel("C7W0K6PNW", "");
+    await bot.say("test");
+}
+async (trigger: any) => {
+
+    // there's a user id somewhere in this trigger
+    let user = trigger.userid;
+
+    // spawn a bot
+    let bot = await controller.spawn();
+    // await bot.startConversationWithUser(user);
+
+    await bot.say('ALERT! A trigger was detected');
+    // await bot.beginDialog(ALERT_DIALOG);
+
+};
+
+test();
