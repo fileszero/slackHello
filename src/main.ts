6513533619068d53ff94e8b60db5e8548820ef3f
FILES=0 <files.eq.zero@gmail.com>
2019-06-19 17:24:43 +0900

    send message

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import dotenv from 'dotenv';
-import { Botkit } from 'botkit';
+import { Botkit, BotkitMessage, BotkitConversation } from 'botkit';
 // import { SlackAdapter } from 'botbuilder-adapter-slack'
-import { SlackAdapter, SlackMessageTypeMiddleware, SlackBotWorker } from 'botbuilder-adapter-slack';
+import { SlackAdapter, SlackMessageTypeMiddleware, SlackBotWorker, SlackAdapterOptions } from 'botbuilder-adapter-slack';
 import { TestAdapter } from 'botbuilder';
+
+// run > ngrok http 3000 --log stdout
+// access https://api.slack.com/apps
+// open app -> Event Subscriptions -> set Request URL http://???????.ngrok.io/api/messages
+//
 
 // Botkit for Slack Class Reference
 // https://botkit.ai/docs/v4/reference/slack.html
@@ -17,15 +22,27 @@ if (!process.env.SLACK_CLIENT_ID) {
 }
 console.log(process.env.SLACK_CLIENT_ID);
 
-const adapter = new SlackAdapter({
+const slackOption: SlackAdapterOptions = {
     // clientId: process.env.SLACK_CLIENT_ID,
     clientSigningSecret: process.env.SLACK_SIGNING_SECRET,
-    botToken: process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN,
+    botToken: process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN,    // https://botkit.ai/docs/v4/platforms/slack.html#multi-team-support
     redirectUri: process.env.SLACK_REDIRECTURI || "",
-}).use(new SlackMessageTypeMiddleware());
+};
+const adapter = new SlackAdapter(slackOption).use(new SlackMessageTypeMiddleware());
 
 const controller = new Botkit({
     adapter: adapter
+});
+
+/**
+ * RTM APIのイベント
+ * ないと「Error: Stale RTM connection, closing RTM」というエラーになる
+ */
+controller.on('rtm_open', async (bot, message) => {
+    console.log('** The RTM api just connected!');
+});
+controller.on('rtm_close', async (bot, message) => {
+    console.log('** The RTM api just closed');
 });
 
 controller.on('message', async (bot, message) => {
@@ -37,31 +54,38 @@ controller.hears('hi', ['direct_message', 'direct_mention', 'mention'], async (b
     await bot.reply(message, 'hello');
 });
 
+controller.hears('test', ['direct_message', 'direct_mention', 'mention'], async (bot, message) => {
+    let msg = message.text || "";
+    msg = msg.replace(/test/i, "").trim();
+    test(msg);
+});
+
 // default
 // 最後に記述してください。
 controller.hears('(.*)', ['direct_message', 'direct_mention', 'mention'], async (bot, message) => {
     await bot.reply(message, 'なに??');
 });
+test("STARTUP");
 
-async function test(): Promise<void> {
+async function test(msg: string): Promise<void> {
+    const send_message = "bot kit message : " + msg + " : " + Date.now().toString();
+    // boot_data.team_id
+    // "T7W20F22G"
     // spawn a bot
-    let bot: SlackBotWorker = await controller.spawn() as SlackBotWorker;
+    let bot: SlackBotWorker = await controller.spawn(msg) as SlackBotWorker;
 
-    await bot.startConversationInChannel("C7W0K6PNW", "");
-    await bot.say("test");
+    // let bot = await controller.spawn() as SlackBotWorker;
+    //await bot.startConversationInChannel("C7W0K6PNW", "");
+    await bot.startPrivateConversation("U7W20F25A"); //  function works only on platforms with multiple channels.
+    // await bot.startConversationWithUser("U7W20F25A");
+    //await bot.say("test");
+    // const im_res = await bot.api.im.open({ user: "U7W20F25A" }) as any;
+    const message: Partial<BotkitMessage> = {
+        text: send_message,
+        channel: "C7W0K6P5G",    // #general
+        // user: "U7W20F25A",
+        // channel: im_res.channel.id,
+    };
+    console.log(send_message);
+    await bot.say(send_message);
 }
-async (trigger: any) => {
-
-    // there's a user id somewhere in this trigger
-    let user = trigger.userid;
-
-    // spawn a bot
-    let bot = await controller.spawn();
-    // await bot.startConversationWithUser(user);
-
-    await bot.say('ALERT! A trigger was detected');
-    // await bot.beginDialog(ALERT_DIALOG);
-
-};
-
-test();
