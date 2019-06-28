849b0915c991d0444bfdd8b4c289571cf2b3fb30
FILES=0 <files.eq.zero@gmail.com>
2019-06-28 15:23:14 +0900

    upload file as reply

--- src/mailConvert.ts
+++ src/mailConvert.ts
@@ -2,6 +2,7 @@ import * as emoji from 'emoji';
 import * as mailparser from 'mailparser';
 import * as fs from 'fs';
 import * as slackBot from './slackBot'
+import { SlackBotWorker } from 'botbuilder-adapter-slack';
 
 const Iconv = require('iconv').Iconv;
 
@@ -27,11 +28,12 @@ function unifiedToSlack(text: string) {
     });
 }
 (async () => {
-    const text = await readStdin();
+    // const text = await readStdin();
     // const text = await readStdin2()
     //const text = fs.readFileSync(0);    //type .\data\original_msg.eml | node ./dest/mailConvert.js
     // .\data\iso-2022-jp.eml
     // const text = fs.readFileSync('./data/iso-2022-jp.eml', {});
+    const text = fs.readFileSync('./data/inline_img.eml', {});
 
     console.log(text);
     const mail_opt: mailparser.SimpleParserOptions = {
@@ -44,12 +46,30 @@ function unifiedToSlack(text: string) {
     unified = emoji.kddiToUnified(unified);
     unified = emoji.softbankToUnified(unified);
     // unified = unifiedToSlack(unified);
-    // const controller = slackBot.controller;
-    // slackBot.sendDirectMessage(controller, process.env.DM_TARGET || '', unified);
+    const controller = slackBot.controller;
+    let bot: SlackBotWorker = await controller.spawn("PROACTIVE") as SlackBotWorker;
+    await bot.startPrivateConversation('U7W20F25A'); //  function works only on platforms with multiple channels.    // fileszero
+    const message = {
+        text: "アタッチのテスト",
+    };
+    console.log(JSON.stringify(message));
+    const thread = await bot.say(unified);
     if (mail_data.attachments) {
-        mail_data.attachments.map((f) => {
-            console.log(f.filename);
-        });
+        //https://qiita.com/stkdev/items/992921572eefc7de4ad8
+        const attachments = await Promise.all(mail_data.attachments.map(async (f) => {
+            // var image = 'data:' + f.contentType + ";base64," + f.content.toString('base64');
+            // f.content.toString(image);
+            const upload = await bot.api.files.upload({
+                // https://api.slack.com/methods/files.upload
+                filename: f.filename,
+                title: f.filename,
+                initial_comment: "アタッチ",
+                file: f.content,
+                channels: 'U7W20F25A',   // general: 'C7W0K6P5G'
+                thread_ts: thread.id,
+                filetype: 'png'  //https://api.slack.com/types/file#file_types
+            });
+        }));
     }
-    console.log(unified);
+    //console.log(unified);
 })();
