f616bb43a248dd74b315c2fda7175e648158ef12
FILES=0 <files.eq.zero@gmail.com>
2019-07-01 16:53:57 +0900

    fix syntax error

--- src/main.ts
+++ src/main.ts
@@ -75,7 +75,7 @@ function startCRONJobs() {
 }
 (async () => {
     startCRONJobs();
-    bot.sendMessage(config.slack.dmTarget, "bot started Direct message"{ icon_emoji: ":robot_face:", as_user: false });
+    bot.sendMessage(config.slack.dmTarget, "bot started Direct message", { icon_emoji: ":robot_face:", as_user: false });
     // bot.sendMessage("C7W0K6P5G", "bot started public channel message general");
     // bot.sendMessage("GKJE67PGC", "bot started private channel message", { icon_emoji: ":woman:", as_user: false });
     // bot.sendMessage("G7WRV4KS7", "bot started private channel message (not memeber)log4js");
