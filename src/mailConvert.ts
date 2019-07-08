f1f5214002ac26f68a355d13e549a3548fceb159
FILES=0 <files.eq.zero@gmail.com>
2019-07-08 09:49:02 +0900

    add subject to message

--- src/mailConvert.ts
+++ src/mailConvert.ts
@@ -1,79 +1,83 @@
 import * as emoji from 'emoji';
 import * as mailparser from 'mailparser';
 import * as fs from 'fs';
-import { slackBot } from './slackBot'
+import { slackBot } from './slackBot';
 import { SlackBotWorker } from 'botbuilder-adapter-slack';
 import config from './config';
 
 const Iconv = require('iconv').Iconv;
 
 async function readStdin(): Promise<Buffer> {
-    return new Promise((resolve, reject) => {
-        const data: any[] = [];
-        process.stdin.on('data', (chunk) => {
-            data.push(chunk);
-        });
+	return new Promise((resolve, reject) => {
+		const data: any[] = [];
+		process.stdin.on('data', (chunk) => {
+			data.push(chunk);
+		});
 
-        process.stdin.on('end', () => {
-            resolve(Buffer.concat(data));
-        });
-    });
+		process.stdin.on('end', () => {
+			resolve(Buffer.concat(data));
+		});
+	});
 }
-
 
 function unifiedToSlack(text: string) {
-    return text.replace(emoji.EMOJI_RE(), function (_, m) {
-        var em = emoji.EMOJI_MAP[m];
+	return text.replace(emoji.EMOJI_RE(), function(_, m) {
+		var em = emoji.EMOJI_MAP[m];
 
-        return ': ' + em[1].replace(' ', '_') + ' :';
-    });
+		return ': ' + em[1].replace(' ', '_') + ' :';
+	});
 }
 (async () => {
-    const text = await readStdin();
-    // const text = await readStdin2()
-    //const text = fs.readFileSync(0);    //type .\data\original_msg.eml | node ./dest/mailConvert.js
-    // .\data\iso-2022-jp.eml
-    // const text = fs.readFileSync('./data/original_msg.eml', {});
-    // const text = fs.readFileSync('./data/iso-2022-jp.eml', {});
-    // const text = fs.readFileSync('./data/inline_img.eml', {});
+	const text = await readStdin();
+	// const text = await readStdin2()
+	//const text = fs.readFileSync(0);    //type .\data\original_msg.eml | node ./dest/mailConvert.js
+	// .\data\iso-2022-jp.eml
+	// const text = fs.readFileSync('./data/original_msg.eml', {});
+	// const text = fs.readFileSync('./data/iso-2022-jp.eml', {});
+	//const text = fs.readFileSync('./data/inline_img.eml', {});
 
-    console.log(text);
-    const mail_opt: mailparser.SimpleParserOptions = {
-        //encoding: "sjis"
-    };
-    const mail_data = await mailparser.simpleParser(text, { Iconv: Iconv });
-    const from_address: string = mail_data.from.value[0]["address"];
-    const emailChannel = config.slack.emailChannel.find((ec) => ec.email == from_address);
-    if (!emailChannel) {
-        return "";
-    }
-    console.log(mail_data.text);
-    // if (mail_data.from.value[0]["address"]) {    }
-    let unified = emoji.docomoToUnified(mail_data.text);
-    unified = emoji.kddiToUnified(unified);
-    unified = emoji.softbankToUnified(unified);
-    // unified = emoji.googleToUnified(unified);
-    //unified = unifiedToSlack(unified);
-    unified = from_address + "\n\n" + unified;
-    console.log(unified);
-    const bot = new slackBot({ disable_webserver: true });
-    const thread = await bot.sendMessage(emailChannel.channel, unified, emailChannel.opt);
-    if (mail_data.attachments) {
-        //https://qiita.com/stkdev/items/992921572eefc7de4ad8
-        const attachments = await Promise.all(mail_data.attachments.map(async (f) => {
-            // var image = 'data:' + f.contentType + ";base64," + f.content.toString('base64');
-            // f.content.toString(image);
-            const upload = await thread.bot.api.files.upload({
-                // https://api.slack.com/methods/files.upload
-                filename: f.filename,
-                title: f.filename,
-                initial_comment: "アタッチ",
-                file: f.content,
-                channels: emailChannel.channel,   // general: 'C7W0K6P5G'
-                thread_ts: thread.activity.id,
-                // filetype: 'png'  //https://api.slack.com/types/file#file_types
-            });
-        }));
-    }
-    //console.log(unified);
+	console.log(text);
+	const mail_opt: mailparser.SimpleParserOptions = {
+		//encoding: "sjis"
+	};
+	const mail_data = await mailparser.simpleParser(text, { Iconv: Iconv });
+	const from_address: string = mail_data.from.value[0]['address'];
+	const emailChannel = config.slack.emailChannel.find((ec) => ec.email == from_address);
+	if (!emailChannel) {
+		return '';
+	}
+	console.log(mail_data.text);
+	// if (mail_data.from.value[0]["address"]) {    }
+	let unified = emoji.docomoToUnified(mail_data.text);
+	unified = emoji.kddiToUnified(unified);
+	unified = emoji.softbankToUnified(unified);
+	// unified = emoji.googleToUnified(unified);
+	//unified = unifiedToSlack(unified);
+	unified = from_address + '\n\n' + unified;
+	if (mail_data.subject) {
+		unified = '*' + mail_data.subject.trim() + '* / ' + unified;
+	}
+	console.log(unified);
+	const bot = new slackBot({ disable_webserver: true });
+	const thread = await bot.sendMessage(emailChannel.channel, unified, emailChannel.opt);
+	if (mail_data.attachments) {
+		//https://qiita.com/stkdev/items/992921572eefc7de4ad8
+		const attachments = await Promise.all(
+			mail_data.attachments.map(async (f) => {
+				// var image = 'data:' + f.contentType + ";base64," + f.content.toString('base64');
+				// f.content.toString(image);
+				const upload = await thread.bot.api.files.upload({
+					// https://api.slack.com/methods/files.upload
+					filename: f.filename,
+					title: f.filename,
+					initial_comment: 'アタッチ',
+					file: f.content,
+					channels: emailChannel.channel, // general: 'C7W0K6P5G'
+					thread_ts: thread.activity.id
+					// filetype: 'png'  //https://api.slack.com/types/file#file_types
+				});
+			})
+		);
+	}
+	//console.log(unified);
 })();
